db55806a23405bdcc0607baca23cf1953b74ecfb
Vilnis Lībeks <s20libeviln@venta.lv>
2022-12-05 23:33:41 +0200

    First commit, empty project and .gitignore

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour
+{
+    public CharacterController controller;
+    private float speed = 5f;
+    public float gravity = 0f;
+    
+    Vector3 velocity;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * x + transform.forward *z;
+        controller.Move(move * speed *Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+    
+}
